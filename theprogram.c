36d43e722adecdc084fc977260b56b4df59d1ba0
Charlene Delfin <68071505+charlenedelfin@users.noreply.github.com>
2023-03-07 17:56:38 +0800

    Update theprogram.c

--- theprogram.c
+++ theprogram.c
@@ -1,43 +1,88 @@
+// Separates words into groups
+
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-  int words = 0, n = 7000;
-  
-  char text[n];
-  printf("");
-  fgets(text, sizeof(text), stdin);
-  
-  for (int i = 0; i < n; i++)
-  {
-    if (isspace(text[i]))
+    // Welcome message
+    printf("Welcome! This program separates words in a given text into groups.\n");
+    printf("\n");
+
+    // Declares text variable
+    char text[7000];
+
+    // Declares other measurements
+    int characters = 0, words = 0, i = 0;
+
+    // Gets text
+    printf("Paste your text below:\n");
+
+    // Saves text into variable
+    fgets(text, sizeof(text), stdin);
+
+    // Measures text
+    while (isalpha(text[i]) || isspace(text[i]) || ispunct(text[i]) || isdigit(text[i]))
     {
-      words++;
-    }
-    if (text[i] == NULL)
-    {
-      break;
-    }
-    if (words >= 8 && text[i] == '.' || words >= 8 && text[i] == '!' || words >= 8 && text[i] == '?')
-    {
-      printf("%c", text[i];
-      printf("\n");
-      i++;
-    }
-    else
-    {
-      if (text[i - 1] == "\n")
-      {
+        characters++;
         i++;
-        printf("%c", text[i]);
-      }
-      else
-      {
-        printf("%c", text[i]);
-      }
     }
-  }
-  return 0;
+
+    // Resets i
+    i = 0;
+
+    printf("\n");
+
+    // Asks for line measurement
+    int wordcount;
+    printf("How many words per group? Numerical answer only.\n");
+    scanf("%i", &wordcount);
+
+    printf("\n");
+
+    // Asks for what to begin each group with
+    char answer;
+    printf("Start each group with a different word? Press y for yes or n for no.\n");
+    scanf(" %c", &answer);
+    printf("\n");
+    char name[747];
+    if (answer == 'y')
+    {
+        printf("Give me the new word:\n");
+        scanf("%s", name);
+        printf("\n");
+        printf("Result:\n");
+        printf("%s: ", name);
+    }
+
+    while (characters > 0)
+    {
+        // Starts printing text
+        printf("%c", text[i]);
+
+        // Counts words
+        if (isspace(text[i]))
+        {
+            words++;
+        }
+
+        // Formats thought
+        if ((words >= wordcount && text[i] == '.') || text[i] == '!' || text[i] == '?')
+        {
+            printf("\n");
+            printf("\n");
+            if (answer == 'y')
+            {
+                printf("%s: ", name);
+            }
+
+            // Skips space
+            i++;
+        }
+        i++;
+        characters--;
+    }
+    return 0;
 }
